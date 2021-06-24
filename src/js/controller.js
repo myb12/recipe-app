3f85e0bbed202b6db2a0c57e6ff9668ebe8ebb10
Mohammad  Yasin <mohammadyasinbappy@outlook.com>
2021-06-24 22:23:57 +0600

    loging hello world to consol

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,5 +126,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Hello world');
 };
 init(); //subscriber function
