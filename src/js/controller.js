c064e11d2d1db429b5c8483400790896d8a8825a
Mohammad  Yasin <mohammadyasinbappy@outlook.com>
2021-06-24 22:25:47 +0600

    message cleaned

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Hello world');
 };
 init(); //subscriber function
